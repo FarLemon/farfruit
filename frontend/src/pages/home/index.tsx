c65c09e364dec74f0313b46ae6c5606612b5777b
Far <farlemon.ffc@gmail.com>
2022-08-20 05:17:00 -0400

    Initial frontend commit

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import { Component } from "solid-js";
+
+import "@styles/pages/_home.scss";
+
+
+
+const Home: Component = () => {
+
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  );
+};
+
+
+
+export default Home;
