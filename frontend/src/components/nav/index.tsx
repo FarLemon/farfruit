d853c423c1a6041eea2b90de0a57c680b3533808
Far <farlemon.ffc@gmail.com>
2022-08-27 22:32:12 -0400

    functional hamburger menu

--- frontend/src/components/nav/index.tsx
+++ frontend/src/components/nav/index.tsx
@@ -24,18 +24,23 @@ const Nav: Component = () => {
 
 
   return (
-    <div class="nav__spacer">
-      <nav class="nav background--gray-6 text--gray-2 text--500 text--sm" onChange={() => {setOpen(false)}}>
-          <SVGlogo class="nav__icon"/>
+    <nav class="nav background--gray-6 text--gray-2 text--500 text--sm" onChange={() => {setOpen(false)}}>
+        <SVGlogo class="nav__icon"/>
+        <button class="nav__hamburger" onClick={() => setOpen(!open())}>
+          <svg class={"nav__hamburger__svg" + (open() ? " active" : "")} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 80">
+            <rect id="top" x="0" y="0" rx="3" width="100" height="6"/>
+            <rect id="middle" x="0" y="37" rx="3" width="100" height="6"/>
+            <rect id="bottom" x="0" y="74" rx="3" width="100" height="6"/>
+          </svg>
+        </button>
 
-          <ul class={"nav__links" + (open() ? " active" : "")}>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/alasgdfk">404</Link>
-          </ul>
-      </nav>
-    </div>
+        <ul class={"nav__links" + (open() ? " active" : "")}>
+          <Link href="/" onClick={() => setOpen(false)}>Home</Link>
+          <Link href="/about" onClick={() => setOpen(false)}>About</Link>
+          <Link href="/company" onClick={() => setOpen(false)}>Company</Link>
+          <Link href="/contact" onClick={() => setOpen(false)}>Contact</Link>
+        </ul>
+    </nav>
   );
 };
 
